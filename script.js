71bc9540325767b4714b76db3d1c2f709df07068
dsh91698 <56885229+dsh91698@users.noreply.github.com>
2022-04-30 18:20:46 +0400

    feat: keyboard frame add

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
 console.log('Virtual keyboard task');
+//===============================================================================//
+function createNewElement(tag, className, text) {
+    const element = document.createElement(tag);
+    element.classList.add(className);
+    element.textContent = text;
+    //element.innerHTML = text;
+    return element;
+}
+
+let header = createNewElement('header', 'header', '');
+let headerText = createNewElement('h1', 'header-text', 'Virtual keyboard task');
+header.append(headerText);
+document.body.prepend(header);
+
+function createKeyboard() {
+    let keyboard = createNewElement('div', 'keyboard', '');
+    const keyboardContainer = createNewElement('div', 'container', '');
+    keyboard.append(keyboardContainer);
+    document.body.append(keyboard);
+}
+
+createKeyboard();
+
